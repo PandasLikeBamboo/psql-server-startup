2dda2cfb5b68b66fcf37a2b527c6f4765cc6a2d2
wikto <w.g.brach@student.utwente.nl>
2024-06-08 00:40:11 +0200

    another attempt on reading and writing a data from external source file

--- pss.bat
+++ pss.bat
@@ -1,5 +1,5 @@
 @echo off
-
+goto init
 rem REMEMBER TO UPLOAD THE config.txt WITH BLANK PATH!!!
 
 rem sets a path to config file 
@@ -10,13 +10,24 @@ for /f "delims=" %%i in (%ConfigFile%) do (
 )
  goto setpath
 
+:modifypath
+echo %DataFolderPath%
+pause
+if "%DataFolderPath%" == "null" goto setpath
+echo Current path to ./data folder: %DataFolderPath%
+echo.
+@set /P "choice=Do you want to change it? (y/n) "
+if "%choice%" == "y" goto setpath
+if "%choice%" == "n" goto main
+echo Invalid choice.
+goto modifypath
 
 :setpath
 echo Paste path to ./data folder in your PostgreSQL server directory:
-@set /P "DataFolderPath="
+@set /P "LocalDataFolderPath="
 rem here set the value of the DataFolderPath variable in the config file
-echo DataFolderPath=%DataFolderPath% > config.txt
-echo Path set to: %DataFolderPath%
+echo DataFolderPath=%LocalDataFolderPath% > %ConfigFile%
+echo Path set to: %LocalDataFolderPath%
 pause
 goto main
 
@@ -27,14 +38,14 @@ echo.
 echo 1 start
 echo 2 stop
 echo 3 restart
-echo 4 set path
+echo 4 modify path
 echo 5 exit (exits cmd)
 echo.
 @set /P "action=User input: "
 if "%action%" == "1" goto start
 if "%action%" == "2" goto stop
 if "%action%" == "3" goto restart
-if "%action%" == "4" goto setpath
+if "%action%" == "4" goto modifypath
 rem add option to reset the path
 if "%action%" == "5" goto exit
 echo Invalid input
@@ -43,19 +54,19 @@ goto main
 :start
 echo Starting the server
 echo.
-pg_ctl -D "%path%" start 
+pg_ctl -D "%DataFolderPath%" start 
 goto main
 
 :stop
 echo Stopping the server
 echo.
-pg_ctl -D "%path%" stop
+pg_ctl -D "%DataFolderPath%" stop
 goto main
 
 :restart
 echo Restarting the server
 echo.
-pg_ctl -D "%path%" restart
+pg_ctl -D "%DataFolderPath%" restart
 goto main	
 
 :exit 
