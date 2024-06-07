a1f132097ebe703b632957ea82ce6186e4db07ae
wikto <w.g.brach@student.utwente.nl>
2024-06-07 23:56:51 +0200

    script now writing the path specified by user to the config.txt file

--- pss.bat
+++ pss.bat
@@ -1,4 +1,23 @@
 @echo off
+
+rem REMEMBER TO UPLOAD THE config.txt WITH BLANK PATH!!!
+
+rem sets a path to config file 
+:init
+set "ConfigFile=config.txt"
+for /f "delims=" %%i in (%ConfigFile%) do (
+	set "%%i"
+)
+ goto setpath
+
+
+:setpath
+echo Paste path to ./data folder in your PostgreSQL server directory:
+@set /P "DataFolderPath="
+rem here set the value of the DataFolderPath variable in the config file
+echo DataFolderPath=%DataFolderPath% > config.txt
+echo Path set to: %DataFolderPath%
+pause
 goto main
 
 :main
@@ -8,34 +27,40 @@ echo.
 echo 1 start
 echo 2 stop
 echo 3 restart
-echo 4 exit (exits cmd)
+echo 4 set path
+echo 5 exit (exits cmd)
 echo.
 @set /P "action=User input: "
 if "%action%" == "1" goto start
 if "%action%" == "2" goto stop
 if "%action%" == "3" goto restart
-if "%action%" == "4" goto exit
+if "%action%" == "4" goto setpath
+rem add option to reset the path
+if "%action%" == "5" goto exit
 echo Invalid input
 goto main
 
 :start
 echo Starting the server
 echo.
-pg_ctl -D "C:\Program Files\PostgreSQL\16\data" start 
+pg_ctl -D "%path%" start 
 goto main
 
 :stop
 echo Stopping the server
 echo.
-pg_ctl -D "C:\Program Files\PostgreSQL\16\data" stop
+pg_ctl -D "%path%" stop
 goto main
 
 :restart
 echo Restarting the server
 echo.
-pg_ctl -D "C:\Program Files\PostgreSQL\16\data" restart
+pg_ctl -D "%path%" restart
 goto main	
 
 :exit 
 pause
 exit /B
+
+:test
+pause
